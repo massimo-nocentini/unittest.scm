9d9511ab1614f31be8c35abd649ea6cf4a4187af
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-02-29 17:49:26 +0100

    Refactoring

--- src/test/bootstrap.scm
+++ src/test/bootstrap.scm
@@ -2,18 +2,19 @@
 (import unittest)
 
 (define-sut wasrun-sut
-    ((setup tc)       (unittest/testcase-wassetup-set! tc #t))
-    ((test-method tc) (unittest/testcase-ran-set! tc #t)))
+    ((setup tc)         (unittest/testcase-wassetup-set! tc #t))
+    ((test-method tc _) (unittest/testcase-ran-set! tc #t)))
 
 (define-sut bootstrap-sut
-    ((test-running tc) (lettest ((t 'test-method))
+    ((setup tc) (lettest ((t 'test-method)) t))
+    ((test-running tc t) 
                         (assert (not (unittest/testcase-ran t)))
                         (unittest/testcase-run t wasrun-sut)
-                        (assert (unittest/testcase-ran t))))
-    ((test-setup tc) (lettest ((t 'test-method))
+                        (assert (unittest/testcase-ran t)))
+    ((test-setup tc t) 
                         (unittest/testcase-run t wasrun-sut)
-                        (assert (unittest/testcase-wassetup t))))                     
-)
+                        (assert (unittest/testcase-wassetup t))))
+
 
 (lettest ((tr 'test-running)
           (ts 'test-setup))
--- src/unittest.scm
+++ src/unittest.scm
@@ -5,19 +5,21 @@
 
     (define-record unittest/testcase ran name wassetup)
 
-    (define (unittest/wasrun name)
-       (make-unittest/testcase #f name #f))
+    (define (unittest/wasrun name) (make-unittest/testcase #f name #f))
 
     (define (unittest/testcase-run testcase methods)
         (let ((setup (alist-ref 'setup methods))
               (teardown (alist-ref 'teardown methods))) 
-            (when setup ((car setup) testcase)))
-        ((car (alist-ref (unittest/testcase-name testcase) methods)) testcase))
+           (let-values ((args (if setup 
+                                ((car setup) testcase)
+                                (values))))
+                (apply (car (alist-ref (unittest/testcase-name testcase) methods))
+                        testcase args))))
 
     (define-syntax define-sut
         (syntax-rules ()
             ((_ sutname ((casename formal ...) body ...) ...) 
-             (define sutname `((casename ,(lambda (formal ...) body ... (void))) ...)))))
+             (define sutname `((casename ,(lambda (formal ...) body ...)) ...)))))
 
     (define-syntax lettest
         (syntax-rules ()
