f60e8b9a0d3692bde50faeedef572c12e8b11c0c
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-03-01 14:13:35 +0100

    Introduce suite.

--- src/unittest.scm
+++ src/unittest.scm
@@ -1,7 +1,7 @@
 
 (module unittest *
 
-	(import scheme (chicken base))
+	(import scheme (chicken base) (chicken condition) (chicken pretty-print))
 
 	(define-record unittest/testcase name log)
 
@@ -10,18 +10,16 @@
     (define (unittest/testcase-logcons! testcase msg)
       (unittest/testcase-log-set! testcase (cons msg (unittest/testcase-log testcase))))
 
-	(define (unittest/testcase-run testcase methods)
+	(define (unittest/testcase-run testcase result methods)
           (let ((setup (alist-ref 'setup methods))
-		(teardown (alist-ref 'teardown methods))) 
-            (let-values ((args (if setup 
-                                   ((car setup) testcase)
-                                   (values))))
-              (apply
-                (car (alist-ref (unittest/testcase-name testcase) methods))
-                testcase args)
-            (when teardown (apply (car teardown) testcase args)))))
+		        (teardown (alist-ref 'teardown methods)))
+            (unittest/result-started! result)
+            (let-values ((args (if setup ((car setup) testcase) (values))))
+                (handle-exceptions exn 
+                    (begin #;(print-error-message  exn) (unittest/result-failed! result))
+                    (apply (car (alist-ref (unittest/testcase-name testcase) methods)) testcase args))
+                (when teardown (apply (car teardown) testcase args)))))
                 
-
 	(define-syntax define-sut
           (syntax-rules ()
             ((_ sutname ((casename formal ...) body ...) ...) 
@@ -31,5 +29,31 @@
           (syntax-rules ()
             ((_ ((test nameexp) ...) body ...)
              (let ((test (unittest/wasrun nameexp)) ...) body ...))))
-	)
+
+    (define-record unittest/result ran failed)
+
+    (define (unittest/result-summary result)
+      `((ran ,(unittest/result-ran result))
+        (failed ,(unittest/result-failed result))))
+
+    (define (unittest/result-started! result)
+      (unittest/result-ran-set! result (add1 (unittest/result-ran result))))
+
+    (define (unittest/result-failed! result)
+      (unittest/result-failed-set! result (add1 (unittest/result-failed result))))
+
+    (define-syntax assert-equal
+      (syntax-rules ()
+        ((_ a b err ...) (assert (equal? a b) err ... `((expected ,a) (got ,b))))))
+
+    (define-syntax letsuite
+      (syntax-rules ()
+        ((_ ((name (method ...)) ...) body ...)
+
+         (letrec ((name (lambda (r sut)
+             (lettest ((method 'method) ...)
+               (unittest/testcase-run method r sut) ...)))...)
+
+            body ...))))
+)
 
