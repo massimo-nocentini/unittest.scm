bc9de88a653d6b183784a6c65fdfb5f66516aa7e
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-04-18 23:05:27 +0200

    Add a test about calling a function from C to Scheme

--- src/test/learning.scm
+++ src/test/learning.scm
@@ -5,19 +5,34 @@
 
 (define-suite learning-suite
 
-    ((test-alist-ref _) 
-        (let ((alst '((a 3) (b 2))))
-        (⊦= '(3) (alist-ref 'a alst))
-        (⊦= '(2) (alist-ref 'b alst))
-        (⊦= #f (alist-ref 'c alst))))
+  ((test-alist-ref _) 
+   (let ((alst '((a 3) (b 2))))
+     (⊦= '(3) (alist-ref 'a alst))
+     (⊦= '(2) (alist-ref 'b alst))
+     (⊦= #f (alist-ref 'c alst))))
 
-    ((test/len _) (let ((my-strlen (foreign-lambda* int ((scheme-object cons)) "C_return(C_header_size(cons));")))
-                    (⊦= 2 (my-strlen (cons 1 '())))
-                    (⊦= 2 (my-strlen (cons 1 (cons 2 (cons 3 '())))))
-                    (⊦= 11 (my-strlen "hello world"))))
+  ((test/len _) (let ((my-strlen (foreign-lambda* int 
+                                                  ((scheme-object cons)) 
+                                                  "C_return(C_header_size(cons));")))
+                  (⊦= 2 (my-strlen (cons 1 '())))
+                  (⊦= 2 (my-strlen (cons 1 (cons 2 (cons 3 '())))))
+                  (⊦= 11 (my-strlen "hello world"))))
 
-    ((test-null-eq? _) (⊨ (eq? '() '())))
-    #;((test-null-car _) (⊦⧳ ((exn)) (car (list))))
-)
+  ((test/c-apply _) 
+   (let ((witness (gensym))
+         (my-strlen (foreign-safe-lambda* scheme-object ((scheme-object f)) 
+					  #<<END
+					  C_word res;
+					  int s = CHICKEN_apply(f, C_SCHEME_END_OF_LIST, &res);
+					  printf("code: %d\n", s);
+					  C_return (res);
+END
+					  )))
+     (⊦= witness (car (my-strlen (lambda () (list witness 4)))))))
+
+  ((test-null-eq? _) (⊨ (eq? '() '())))
+  #;((test-null-car _) (⊦⧳ ((exn)) (car (list))))
+  )
 
 (unittest/✓ learning-suite)
+
